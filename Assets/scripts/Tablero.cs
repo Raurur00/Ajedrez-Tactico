0188c2e0df2c71ca0772d904b34ec1d221621ea6
Raurur00 <javierzavalatorres@gmail.com>
2019-05-30 17:45:48 -0400

    Crear casillas desde archivo

--- Assets/scripts/Tablero.cs
+++ Assets/scripts/Tablero.cs
@@ -14,35 +14,28 @@ public class Tablero : MonoBehaviour {
 	}
 
     public void createTablero(){
+        //Abrir el archivo
         StreamReader objReader = new StreamReader("./Assets/tableros/default.txt");
-        string sLine = "";
-        ArrayList arrText = new ArrayList();
-        sLine = objReader.ReadLine();
+        //Extraer Ancho y Largo de la matriz del archivo
+        string sLine = objReader.ReadLine();
         string[] linea = sLine.Split(' ');
         int Ancho = Int32.Parse(linea[0]);
-        int Alto = Int32.Parse(linea[1]);
-        tablero = new GameObject[Ancho,Alto];
-        while (sLine !=null) {
-            sLine = objReader.ReadLine();
-            if (sLine != null) {
+        int Largo = Int32.Parse(linea[1]);
+        //Crear arreglo con Ancho y Largo
+        tablero = new GameObject[Ancho,Largo];
+        for(int i = 0;i<Ancho;i++) {
+			sLine = objReader.ReadLine();
+            if (sLine!=null){
                 string[] numbers = sLine.Split(' ');
-                arrText.Add(numbers);
-            };
-        }
-        objReader.Close();
-        for(int i = 0;i<Ancho;i++){
-			for(int j=0;j<Alto;j++){
-				GameObject casillaTemp = Instantiate(CasillaPrefab,new Vector3(i,0,j),Quaternion.identity);
-				tablero[i,j] = casillaTemp;
-			}
-		}
-        /*foreach (string[] sOutputs in arrText) { 
-            foreach (string sOutput in sOutputs) {
-                if (sOutput.Equals("0")) {
-                    GameObject casillaTemp = Instantiate(CasillaPrefab,new Vector3(i,0,j),Quaternion.identity);
-				    tablero[i,j] = casillaTemp;
+                for(int j=0;j<Largo;j++) {
+                    if (numbers[j].Equals("0")) {
+                        GameObject casillaTemp = Instantiate(CasillaPrefab, new Vector3(i,0,j), Quaternion.identity);
+                        tablero[i,j] = casillaTemp;
+                    }
                 }
             }
-        }*/
+            
+            
+		}
     }
 }
